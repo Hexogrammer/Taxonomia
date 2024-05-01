5064f1e15fa9ced9200407281d44dc44fa0b2444
Frederik <robofred@kabelmail.de>
2024-05-01 12:39:06 +0200

    Semi-well functioning image hints

--- Taxonomia.py
+++ Taxonomia.py
@@ -1,17 +1,20 @@
-import taxoniq
 from random import randint
 import xerox
 import sys
 from math import sqrt
 from time import sleep
 from tabulate import tabulate
+import urllib
 
+import taxoniq
+import pyinaturalist
 import wikipedia
 import webbrowser
 
-from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtCore import Qt, QTimer, QUrl
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QKeySequence
+from PyQt5.QtGui import QKeySequence, QPixmap
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest
 
 def lowest_commonality(taxon1, taxon2):
     pass
@@ -27,6 +30,17 @@ def str_with_plus(num):
         return '+' + str(num)
     else:
         return str(num)
+
+def get_image_hint(taxon):
+    try:
+        name = taxon.common_name
+    except:
+        name = taxon.scientific_name
+    observations = pyinaturalist.get_observations(taxon_name=taxon.scientific_name, photos=True)["results"]
+    print(name, len(observations))
+    if observations:
+        image_url = observations[randint(0, len(observations)-1)]["taxon"]["default_photo"]["medium_url"]
+        return image_url
 
 class OpeningDialog(QDialog):
     def __init__(self):
@@ -137,6 +151,26 @@ Guess history:
         self.copy_button.setText("Copied")
         self.copy_button.setDisabled(True)
         QTimer.singleShot(1000, lambda: [self.copy_button.setText("Share on Discord"), self.copy_button.setDisabled(False)])
+
+class HintDialog(QDialog):
+    def __init__(self, last_correct_name, next_correct_rank, image_url):
+        super().__init__()
+        self.setWindowTitle("Taxonomia")
+        self.setWindowModality(Qt.NonModal)
+        self.vert_layout = QVBoxLayout()
+        self.setLayout(self.vert_layout)
+        self.vert_layout.addWidget(QLabel(f"Random species of the correct {next_correct_rank} (after {last_correct_name})."))
+
+        data = urllib.request.urlopen(image_url).read()
+        pixmap = QPixmap()
+        pixmap.loadFromData(data)
+        self.image_label = QLabel()
+        self.image_label.setPixmap(pixmap)
+        self.vert_layout.addWidget(self.image_label)
+        print(image_url)
+        print(pixmap)
+        print(self.image_label)
+
 class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
@@ -144,7 +178,9 @@ class MyWidget(QWidget):
 
         self.tries = 0
         self.try_list = []
+        self.next_correct = None
         self.last_correct = None
+        self.last_ind = 0
         self.last_accuracy = 1
 
         self.main_layout = QHBoxLayout()
@@ -158,11 +194,15 @@ class MyWidget(QWidget):
         self.line_edit.textChanged.connect(self.on_text_update)
         self.horizontal_layout.addWidget(self.line_edit)
 
-        self.button = QPushButton('Enter')
-        self.button.setShortcut(QKeySequence(Qt.Key_Return))
-        self.button.setDisabled(True)
-        self.button.clicked.connect(self.on_enter)
-        self.horizontal_layout.addWidget(self.button)
+        self.enter_button = QPushButton("Enter")
+        self.enter_button.setShortcut(QKeySequence(Qt.Key_Return))
+        self.enter_button.setDisabled(True)
+        self.enter_button.clicked.connect(self.on_enter)
+        self.horizontal_layout.addWidget(self.enter_button)
+
+        self.hint_button = QPushButton("Hint")
+        self.hint_button.clicked.connect(self.give_hint)
+        self.horizontal_layout.addWidget(self.hint_button)
 
         self.vert_layout.addLayout(self.horizontal_layout)
 
@@ -170,7 +210,7 @@ class MyWidget(QWidget):
         self.text_edit.setReadOnly(True)
         self.vert_layout.addWidget(self.text_edit)
 
-        self.giveup_button = QPushButton('Give up (looser)')
+        self.giveup_button = QPushButton("Give up (looser)")
         self.giveup_button.clicked.connect(self.on_giveup)
         self.vert_layout.addWidget(self.giveup_button)
 
@@ -200,9 +240,9 @@ class MyWidget(QWidget):
         guess_species = None
         try:
             guess_species = taxoniq.Taxon(scientific_name=guess_text)
-            self.button.setDisabled(False)
+            self.enter_button.setDisabled(False)
         except:
-            self.button.setDisabled(True)
+            self.enter_button.setDisabled(True)
     def on_enter(self):
         self.text_edit.append("---------------------")
         guess_text = self.line_edit.text()
@@ -213,12 +253,15 @@ class MyWidget(QWidget):
             self.text_edit.append("Try " + str(self.tries) + ": " + guess_text)
             ind = 0
             for target_clade, guess_clade in zip(reversed(guess_species.lineage), reversed(self.target.lineage)):
-                ind += 1
                 if target_clade.scientific_name == guess_clade.scientific_name:
                     self.text_edit.append(taxon_to_message(guess_clade))
                     self.last_correct = guess_clade
+                    self.last_ind = ind
+                    if not self.last_correct == self.target:
+                        self.next_correct = list(reversed(self.target.lineage))[ind + 1]
                 else:
                     break
+                ind += 1
             accuracy_message = f"{str(len(self.last_correct.lineage))}/{str(len(self.target.lineage))}"
             self.try_list.append([str(self.tries), taxon_to_message(guess_species), taxon_to_message(self.last_correct), accuracy_message, str_with_plus(ind - self.last_accuracy)])
             if ind > self.last_accuracy:
@@ -244,6 +287,7 @@ class MyWidget(QWidget):
 
     def on_giveup(self):
         self.text_edit.append("---------------------")
+        self.text_edit.append(f"genus-code: {str(self.target.tax_id ** 2)}")
         self.text_edit.append("You failed, it was:")
         for taxon in reversed(self.target.lineage):
             try:
@@ -260,6 +304,20 @@ class MyWidget(QWidget):
                 webbrowser.open(self.last_correct.wikidata_url)
             except:
                 self.text_edit.append("Sry, no Wiki article available")
+    def give_hint(self):
+        for correct in list(reversed(self.target.lineage))[self.last_ind+1:]:
+            image_url = get_image_hint(correct)
+            if image_url:
+                break
+        if image_url:
+            self.text_edit.append("---------------------")
+            self.text_edit.append("Try " + str(self.tries) + ": image hint")
+            self.tries += 1
+            self.try_list.append([str(self.tries), "image hint", "", "", ""])
+            self.dialog = HintDialog(self.last_correct.scientific_name, str(self.next_correct.rank)[5:], image_url)
+            self.dialog.exec()
+        else:
+            self.text_edit.append("sry, no images available")
 
 app = QApplication(sys.argv)
 widget = MyWidget()
